c4f3478d0b097225bc9561c0c5a75b49b0df912e
Fiona Klute <fiona.klute@gmx.de>
2020-10-11 15:39:33 +0200

    Search clean-cppcheck-xml.py relative to action.bash

--- action.bash
+++ action.bash
@@ -9,7 +9,7 @@ output="$(realpath "${INPUT_SARIF_FILE}")"
 # understand
 if [ "${tool,,}" = "cppcheck" ]; then
     tmp=$(tempfile)
-    python3 clean-cppcheck-xml.py "${input}" >"${tmp}"
+    python3 "$(dirname ${0})"/clean-cppcheck-xml.py "${input}" >"${tmp}"
     input="$(realpath "${tmp}")"
 fi
 
