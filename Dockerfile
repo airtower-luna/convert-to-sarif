637ca1253643ad4372afec6ed903b887349d629a
Fiona Klute <fiona.klute@gmx.de>
2020-10-23 20:26:58 +0200

    Require sarif-multitool 2.3.7, remove special handling for CppCheck
    Sarif-multitool 2.3.7 fixes the bug that required the special
    handling, see https://github.com/microsoft/sarif-sdk/issues/2103

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:12
-COPY action.bash clean-cppcheck-xml.py package.json /action/
+COPY action.bash package.json /action/
 WORKDIR /action/
 RUN npm install
 ENTRYPOINT ["/action/action.bash"]
--- action.bash
+++ action.bash
@@ -5,14 +5,6 @@ tool="${INPUT_TOOL}"
 input="$(realpath "${INPUT_INPUT_FILE}")"
 output="$(realpath "${INPUT_SARIF_FILE}")"
 
-# actual cppcheck output contains a few items the converter doesn't
-# understand
-if [ "${tool,,}" = "cppcheck" ]; then
-    tmp=$(tempfile)
-    python3 "$(dirname ${0})"/clean-cppcheck-xml.py "${input}" >"${tmp}"
-    input="$(realpath "${tmp}")"
-fi
-
 # must be in the action directory to use the npm packages installed there
 cd "$(dirname ${0})"
 npx "@microsoft/sarif-multitool" convert "${input}" \
